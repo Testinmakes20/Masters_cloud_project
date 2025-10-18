ffdcd36b9198eca54e3e04545e2f62f51bb429cb
Pooja <145401427+Testinmakes20@users.noreply.github.com>
2025-10-18 10:16:48 +0000

    Update app.py

--- user_service/app.py
+++ user_service/app.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, HTTPException
-import psycopg2
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 import os
 
 app = FastAPI()
@@ -8,6 +9,17 @@ DB_HOST = os.getenv("DB_HOST", "postgres-service")
 DB_NAME = os.getenv("DB_NAME", "ecommerce")
 DB_USER = os.getenv("DB_USER", "admin")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "admin123")
+
+# app.mount("/static", StaticFiles(directory="frontend"), name="static")
+
+@app.get("/")
+async def root():
+    index_path = os.path.join("frontend", "index.html")
+    with open(index_path, "r") as file:
+        html_content = file.read()
+    return HTMLResponse(content=html_content, status_code=200)
+
+
 
 def get_conn():
     return psycopg2.connect(host=DB_HOST, database=DB_NAME,
